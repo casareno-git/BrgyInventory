df2c69f8df739f929e51ff4073f50afab0e598f3
icezcnt <francisnice1478@gmail.com>
2026-09-17 21:27:02 +0800

    added sison test

---
+++ app/src/test/java/com/example/brgyinventory/quarter2/sisonprofile.java
@@ -0,0 +1,18 @@
+package com.example.brgyinventory.quarter2;
+import org.junit.Test;
+
+public class sisonprofile
+{
+    @Test
+    public void printMyProfile() {
+        String myName = "Jake";
+        String petName = "marshal";
+        String favFood = "adobo";
+        int myAge = 18;
+
+        System.out.println(" Welcome to my Introduction! ");
+        System.out.println("HII!, my name is " + myName + ", and i am " + myAge + " years old ");
+        System.out.println("I have this cute dog named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " everyday");
+    }
+}
