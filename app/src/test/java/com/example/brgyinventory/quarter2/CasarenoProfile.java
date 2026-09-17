e0d2f8014978cb2c0ecd093496cd23e205e38416
casareno-git <21-00222@mcstayuman.edu.ph>
2026-09-17 15:52:53 +0800

    Initial commit

---
+++ app/src/test/java/com/example/brgyinventory/quarter2/CasarenoProfile.java
@@ -0,0 +1,19 @@
+package com.example.brgyinventory.quarter2;
+import org.junit.Test;
+
+public class CasarenoProfile {
+    @Test
+    public void printMyProfile() {
+        // INPUT - Storing personal details in variables
+        String myName = "Ysha";
+        String petName = "Pikachu";
+        String favFood = "Gyudon";
+        int myAge = 18;
+
+        // OUTPUT - Printing to the console
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello. My name is " + myName + ", and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
+    }
+}
