45f357f8886c06fa77249af66bec69c99f458bf3
klydecapalad <klydecapalad3@gmail.com>
2026-09-17 18:17:20 +0800

    Added Klyde's profile test

---
+++ app/src/test/java/com/example/brgyinventory/quarter2/capaladprofile.java
@@ -0,0 +1,17 @@
+package com.example.brgyinventory.quarter2;
+import org.junit.Test;
+
+public class capaladprofile {
+    @Test
+    public void printMyProfile() {
+        String myName = "Klyde";
+        String petName = "Kye";
+        String favFood = "Sinigang";
+        int myAge = 17;
+
+        System.out.println(" Welcome to my Introduction! ");
+        System.out.println("HII!, my name is " + myName + ", and i am " + myAge + " years old ");
+        System.out.println("I have this cute dog named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " 24/7 for sure!");
+    }
+}
